93759ac6dbe05b489915af12519108095cd85713
Moshood2212 <Memud3000@gmail.com>
2023-05-30 07:43:41 -0700

    learning js

---
+++ app.js
@@ -0,0 +1,83 @@
+console.log("hello world");
+
+// js variables and valus ( let and const)
+let x = 2;
+const y = 4;
+console.log(x);
+console.log(y);
+
+const country = "Ghana"
+let state = "Lagos state"
+
+console.log(country);
+console.log(state);
+state = "abia state"
+console.log(state);
+
+
+const firstName = "john";
+const LastName = 'DOe';
+const middleName = 'OWen';
+const currentJob = "Product mananger";
+const email = "memud3000@gmail.com";
+
+// String concatenation joining of strings +
+const fullName = firstName + " " + middleName + " " +LastName;
+console.log(fullName);
+const description = "My name is" + firstName + "i work as a" + currentJob;
+console.log(description);
+
+
+const olu = "you  can message  on  this  email, memud@gamil.com;"
+console.log(olu);
+
+// getting character -
+console.log(middleName[1]);
+console.log(middleName[1]);
+
+
+// string length 
+console.log(firstName.length);
+console.log( fullName.toUpperCase());
+
+// extracting parts of a string-slice substring 
+const email2 = "memud3002gmail.com";
+console.log(email2.slice(0, 5));
+
+
+// string.substrings( start, count of cters)
+console.log(email2.substr(0, 12));
+
+
+// replace string content-replace replaceall
+console.log(email2.replace("a", "o"));
+console.log(email2.replace("memud","toyin"));
+
+// includes, indexof, lastindexof,trim,concat
+// trimstart,trimend,startwith,endswith
+
+// template literals
+const author = "Chinua Achebe";
+const book = "things fall apart";
+const year = 1996;
+
+
+const sent = "the book"+ book 
+console.log(sent);
+
+
+
+
+const
+
+
+
+
+
+
+
+
+
+
+
+
