3f65540e538aff7ab4929f9874dce3ab78488aa2
Moshood2212 <Memud3000@gmail.com>
2023-05-31 07:50:13 -0700

    update

--- app.js
+++ app.js
@@ -1,83 +1,108 @@
-console.log("hello world");
+// console.log("hello world");
 
-// js variables and valus ( let and const)
-let x = 2;
-const y = 4;
-console.log(x);
-console.log(y);
+// // js variables and valus ( let and const)
+// let x = 2;
+// const y = 4;
+// console.log(x);
+// console.log(y);
 
-const country = "Ghana"
-let state = "Lagos state"
+// const country = "Ghana"
+// let state = "Lagos state"
 
-console.log(country);
-console.log(state);
-state = "abia state"
-console.log(state);
-
-
-const firstName = "john";
-const LastName = 'DOe';
-const middleName = 'OWen';
-const currentJob = "Product mananger";
-const email = "memud3000@gmail.com";
-
-// String concatenation joining of strings +
-const fullName = firstName + " " + middleName + " " +LastName;
-console.log(fullName);
-const description = "My name is" + firstName + "i work as a" + currentJob;
-console.log(description);
+// console.log(country);
+// console.log(state);
+// state = "abia state"
+// console.log(state);
 
 
-const olu = "you  can message  on  this  email, memud@gamil.com;"
-console.log(olu);
+// const firstName = "john";
+// const LastName = 'DOe';
+// const middleName = 'OWen';
+// const currentJob = "Product mananger";
+// const email = "memud3000@gmail.com";
 
-// getting character -
-console.log(middleName[1]);
-console.log(middleName[1]);
-
-
-// string length 
-console.log(firstName.length);
-console.log( fullName.toUpperCase());
-
-// extracting parts of a string-slice substring 
-const email2 = "memud3002gmail.com";
-console.log(email2.slice(0, 5));
+// // String concatenation joining of strings +
+// const fullName = firstName + " " + middleName + " " +LastName;
+// console.log(fullName);
+// const description = "My name is" + firstName + "i work as a" + currentJob;
+// console.log(description);
 
 
-// string.substrings( start, count of cters)
-console.log(email2.substr(0, 12));
+// const olu = "you  can message  on  this  email, memud@gamil.com;"
+// console.log(olu);
+
+// // getting character -
+// console.log(middleName[1]);
+// console.log(middleName[1]);
 
 
-// replace string content-replace replaceall
-console.log(email2.replace("a", "o"));
-console.log(email2.replace("memud","toyin"));
+// // string length 
+// console.log(firstName.length);
+// console.log( fullName.toUpperCase());
 
-// includes, indexof, lastindexof,trim,concat
-// trimstart,trimend,startwith,endswith
-
-// template literals
-const author = "Chinua Achebe";
-const book = "things fall apart";
-const year = 1996;
+// // extracting parts of a string-slice substring 
+// const email2 = "memud3002gmail.com";
+// console.log(email2.slice(0, 5));
 
 
-const sent = "the book"+ book 
-console.log(sent);
+// // string.substrings( start, count of cters)
+// console.log(email2.substr(0, 12));
 
 
+// // replace string content-replace replaceall
+// console.log(email2.replace("a", "o"));
+// console.log(email2.replace("memud","toyin"));
 
+// // includes, indexof, lastindexof,trim,concat
+// // trimstart,trimend,startwith,endswith
 
-const
+// // template literals
+// const author = "Chinua Achebe";
+// const book = "things fall apart";
+// const year = 1996;
 
 
 
 
 
 
+// // const countryy = Nigeria;
+// //    const continent = Africa;
+// // let population = 1000000;
+
+// // console.log(country,continent,population);
 
 
+// // let island = "isreal"
+// // console.log();
+    
+
+// const language = "french";
+// console.log(language);
+
+// const countdown = history;
+// const downn = "asray";
+// population = 5000;
+// population /= 2;
+// console.log(population);
+// population += 1
+// console.log(population);
+
+// const des = 
 
 
+const country = "nigeria";
+const continent = "africa";
+let population = 5000;
+console.log(country, continent, population);
+const des = `${country} is in ${continent} `
 
 
+// comparesion operators
+// typeoff
+
+// logical operators   and&  or||    not!
+
+const vip = "Hello world";
+console.log(vip);
+.console.log(vip.endsWith(""));
