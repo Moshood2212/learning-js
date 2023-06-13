7c3160a06413e440c5ab28b7f81969c1a007cc2e
Moshood2212 <Memud3000@gmail.com>
2023-06-13 06:17:44 -0700

    new

---
+++ Script.js
@@ -0,0 +1,18 @@
+// Select element on the webpage-class, id, tagname,css,selectors
+// document.getElementsByClassName
+// document.getElementsByTagName
+// document.getElementById
+
+// document.querySelector
+// document.querySelectorAll
+
+// changing content on the web - innertext, textcontent, innerhtml
+
+
+
+// const body = document.querySelector("body")
+// body.innerHTML = "<h1>javascript</h1>"
+// body.style.backgroundColor = "blue"
+// classlist-- add remove
+
+// responding to users- interaction - event listener , event handler
